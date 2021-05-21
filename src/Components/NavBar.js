63539ad04d9312781a445d0829d0a48cd27ef85b
Idan Entin <idanen@gmail.com>
2021-05-21 13:21:42 +0300

    fix: remove console errors

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,32 +1,30 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import aboutImg from "./../Images/logo.png";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import aboutImg from './../Images/logo.png';
 
 function NavBar() {
   return (
     <nav>
-      <div class="nav-wrapper">
-        <img src={aboutImg} className="App-logo" alt="logo" />
+      <div className='nav-wrapper'>
+        <img src={aboutImg} className='App-logo' alt='logo' />
         <div></div>
 
-        <button class="emergencyExit false" tanindex="0">
-          לסגירת הטופס
-        </button>
-        <ul id="nav-mobile" class="left hide-on-med-and-down">
+        <button className='emergencyExit false'>לסגירת הטופס</button>
+        <ul id='nav-mobile' className='left hide-on-med-and-down'>
           <li>
-            <Link to="/">Home</Link>
+            <Link to='/'>Home</Link>
           </li>
           <li>
-            <Link to="/feel-good">Feel Good</Link>
+            <Link to='/feel-good'>Feel Good</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link to='/about'>About</Link>
           </li>
           <li>
-            <Link to="/Menu">Menu</Link>
+            <Link to='/Menu'>Menu</Link>
           </li>
         </ul>
-        <div class="timerNote">לביטחונך השאלון מאובטח ויסגר תוך:</div>
+        <div className='timerNote'>לביטחונך השאלון מאובטח ויסגר תוך:</div>
       </div>
     </nav>
   );
