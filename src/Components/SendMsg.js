198bce0b682036d2a95fc7ef81c9329cb5eea9ec
Limor Segal <segal.limor@gmail.com>
2021-05-30 09:03:25 +0300

    add send whatsapp msg to all stages and change to appropriate text (small changes) in stage pages

--- src/Components/SendMsg.js
+++ src/Components/SendMsg.js
@@ -1,20 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import globalUtil from '../Utils/globalUtil'
 
 function SendMsg(props) {
     const [txtMsg, setTxtMsg] = useState(props.msg);
+    const focusSendMagTxt = useRef();
+
 
     const sendWhatsApp = () =>{
         //window.open(`https://wa.me?text=${txtMsg}`)
         globalUtil.sendWhatsAppMsg(txtMsg);
     }
 
+    useEffect(() => {
+        if(focusSendMagTxt.current){
+            focusSendMagTxt.current.focus(); 
+        }
+    }, [focusSendMagTxt]);
+
     return (
         <div>
             <form>
                 <div class="form-group purple-border">
-                    <textarea class="form-control" id="exampleInputMessage1" rows="10" value={txtMsg} onChange={(e) => setTxtMsg(e.target.value)} />
+                    <textarea ref={focusSendMagTxt} class="sendMsgToFriend" id="exampleInputMessage1"  value={txtMsg} onChange={(e) => setTxtMsg(e.target.value)} />
                 </div>
                 <button type="button" class="btn btn-primary sendBtn" onClick={() => sendWhatsApp()}>שלחי הודעת whatsapp</button>
             </form>
