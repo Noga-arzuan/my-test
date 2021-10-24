be4c2745e7979f7220ed0eec1a5592d8651c58c1
Noga-Arzuan <63960775+Noga-arzuan@users.noreply.github.com>
2021-10-24 09:18:41 +0300

    Update Menu.js
    change algo

--- src/Pages/Menu/Menu.js
+++ src/Pages/Menu/Menu.js
@@ -12,7 +12,7 @@ export default function Menu() {
   const handleAnswerOptionClick = (isCorrect) => {
     if (isCorrect) setScore(score + 1);
     const nextQuestion = currentQuestion + 1;
-    if (score < 7) setCurrentQuestion(nextQuestion);
+    if (score < 5) setCurrentQuestion(nextQuestion);
     else {
       setShowScore(true);
     }
