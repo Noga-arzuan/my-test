53d6c834bae565ecf9e36159113edfd4ef4ba91b
Noga-Arzuan <63960775+Noga-arzuan@users.noreply.github.com>
2021-11-01 12:27:46 +0200

    Update NavBar.js

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -17,37 +17,41 @@ function NavBar() {
   });
   
   return (
+    <div>
+    האתר בפיתוח 
+    לפרטים נוספים   nogaarzuan@gmail.com
+    </div>
 
-    <nav>
-      <div className='nav-wrapper' style={{ display: "flex", flexDirection: "column", justifyContent: "space-evenly" }}>
-        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+//     <nav>
+//       <div className='nav-wrapper' style={{ display: "flex", flexDirection: "column", justifyContent: "space-evenly" }}>
+//         <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
 
-        <button className='emergencyExit false wingsBtn' onClick={() => emergencyExit()}>לסגירת הטופס</button>
-
-
-          <img src={aboutImg} className='App-logo' alt='logo' />
-        </div>
-        <div className='timerNote'>      לביטחונך השאלון מאובטח ויסגר תוך 10 דקות </div>
+//         <button className='emergencyExit false wingsBtn' onClick={() => emergencyExit()}>לסגירת הטופס</button>
 
 
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <ul id='nav-mobile' className='left hide-on-med-and-down'>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            {/* <li>
-              <Link to='/feel-good'>Feel Good</Link>
-            </li> */}
-            <li>
-              <Link to='/about'>About</Link>
-            </li>
-            {/* <li>
-              <Link to='/Menu'>Menu</Link>
-            </li> */}
-          </ul>
-        </div>
-      </div>
-    </nav>
+//           <img src={aboutImg} className='App-logo' alt='logo' />
+//         </div>
+//         <div className='timerNote'>      לביטחונך השאלון מאובטח ויסגר תוך 10 דקות </div>
+
+
+//         <div style={{ display: "flex", justifyContent: "space-between" }}>
+//           <ul id='nav-mobile' className='left hide-on-med-and-down'>
+//             <li>
+//               <Link to='/'>Home</Link>
+//             </li>
+//             {/* <li>
+//               <Link to='/feel-good'>Feel Good</Link>
+//             </li> */}
+//             <li>
+//               <Link to='/about'>About</Link>
+//             </li>
+//             {/* <li>
+//               <Link to='/Menu'>Menu</Link>
+//             </li> */}
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
 
   );
 
