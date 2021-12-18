682f6c81456a114d03b807a76b08def5c38fd18e
Noga-Arzuan <63960775+Noga-arzuan@users.noreply.github.com>
2021-12-18 19:26:58 +0200

    Update Feel1.js

--- src/Components/Feel1/Feel1.js
+++ src/Components/Feel1/Feel1.js
@@ -11,11 +11,11 @@ export default function Feel1(props) {
   const [score, setScore] = useState(0);
   const handleAnswerOptionClick = (isCorrect) => {
     if (isCorrect) {
-      setScore(score + 1);
+      setScore(score + 5);
     }
 
     const nextQuestion = currentQuestion + 1;
-    if (score < 8) {
+    if (score < 20) {
       setCurrentQuestion(nextQuestion);
     } else {
 
