d835dd2bc4d16941eaa1aafe6bf7e599490145b7
Noga-Arzuan <63960775+Noga-arzuan@users.noreply.github.com>
2021-12-18 19:19:57 +0200

    Update baseQuestions.js

--- src/Pages/Menu/baseQuestions.js
+++ src/Pages/Menu/baseQuestions.js
@@ -1,77 +1,105 @@
-
 export const questions = [
   {
     questionText:
-      'אני מרגישה שאני נמצאת במערכת יחסים שבה אני יכולה להביע את עצמי',
+      'בן הזוג שלי מתרגז אם איני מסכימה איתו     ',
     answerOptions: [
-      { answerText: 'יכולה', isCorrect: false },
-      { answerText: 'לא יכולה', isCorrect: true },
+      { answerText: ' אוי ואבוי לי אם לא נסכים', isCorrect: true },
+      { answerText: ' פתוח לשמוע את דעתי ', isCorrect: false },
     ],
   },
 
   {
-    questionText: 'אני מרגישה שאין לי זמן לעשות  דברים שאני אוהבת  לעשות',
+    questionText: 'בן זוגי גורם לי אשמה ובושה לעיתים קרובות',
     answerOptions: [
-      { answerText: 'אין לי זמן', isCorrect: true },
-      { answerText: 'יש לי זמן', isCorrect: false },
+      { answerText: 'כל הזמן', isCorrect: true },
+      { answerText: 'ממש לא  ', isCorrect: false },
     ],
   },
 
   {
-    questionText: 'אני מרגישה שבן זוגי תומך בי ובמעשים שלי',
+    questionText: 'אין לי סיכוי להצליח בלי בן הזוג שלי ',
     answerOptions: [
-      { answerText: 'תומך', isCorrect: false },
-      { answerText: 'לא תומך', isCorrect: true },
+      { answerText: 'מצליחה רק בזכותו' , isCorrect: false },
+      { answerText: 'ההצלחה שלי לא תלויה בו ', isCorrect: true },
     ],
   },
 
   {
-    questionText: 'בן הזוג שלי לא  אוהב לשמוע מה שאני חושבת',
+    questionText: 'כשאני מצליחה בן הזוג שלי...',
     answerOptions: [
-      { answerText: 'אוהב לשמוע', isCorrect: true },
-      { answerText: 'לא אוהב לשמוע', isCorrect: false },
+      { answerText: 'מזלזל בי ', isCorrect: true },
+      { answerText: '  מפרגן לי ', isCorrect: false },
     ],
   },
 
   {
     questionText:
-      'בן זוגי  מפרגן  לי שיש לי פעילויות  שלא קשורות למערכת היחסים שלנו',
+      'לבן הזוג שלי חשוב לשמוע את דעתי ',
+    answerOptions: [
+      { answerText: ' לא חשוב לו בכלל', isCorrect: true },
+      { answerText: 'חשוב לו מאוד ', isCorrect: false },
+    ],
+  },
+
+  {
+    questionText:
+      'בן הזוג שלי מעודד אותי לעשות דברים שלא קשורים לזוגיות שלנו',
+    answerOptions: [
+      { answerText: ' אוהב שאני רק איתו', isCorrect: true },
+      { answerText: 'מעודד לגמרי', isCorrect: false },
+    ],
+  },
+
+  {
+    questionText:
+      'בן הזוג שלי מתעקש לבוא איתי כשאני נפגשת עם חברות, ידידים או המשפחה הקרובה שלי',
+    answerOptions: [
+      { answerText: 'מתעקש', isCorrect: true },
+      { answerText: 'לא מתעקש', isCorrect: false },
+    ],
+  },
+
+ 
+
+  {
+    questionText:
+     'בן הזוג שלי מקנא לי',
+    answerOptions: [
+      { answerText: 'סומך עליי בעיניים עצומות', isCorrect: false },
+      { answerText: 'כל הזמן ועל כל דבר ', isCorrect: true },
+    ],
+  },
+  {
+    questionText:'אני נפגשת עם חברים גם בלי בן הזוג שלי',
+
+    answerOptions: [
+      { answerText: ' תמיד איתו ', isCorrect: true },
+      { answerText: 'איך שבא לי ', isCorrect: false },
+    ],
+  },
+  
+   
+  {
+    questionText:
+      'אני מרגישה שאני יכולה להצליח בהרבה  דברים בלי בן הזוג שלי ',
+    answerOptions: [
+      { answerText: 'יכולה להצליח', isCorrect: false },
+      { answerText: 'לא יכולה להצליח', isCorrect: true },
+    ],
+  },
+
+  {
+    questionText: 'בן הזוג מפרגן לי על ההצלחות שלי',
     answerOptions: [
       { answerText: 'מפרגן', isCorrect: false },
       { answerText: 'לא מפרגן', isCorrect: true },
     ],
   },
-
   {
-    questionText: 'בן הזוג שלי שולח לי המון הודעות במשך היום',
+    questionText: 'בן הזוג שלי חושד בי כל הזמן שאני לא נאמנה לו',
     answerOptions: [
-      { answerText: 'שולח', isCorrect: true },
-      { answerText: 'לא שולח', isCorrect: false },
-    ],
-  },
-
-  {
-    questionText: ' לי ולבן זוגי אין כמעט ריבים ',
-    answerOptions: [
-      { answerText: 'אין ריבים', isCorrect: false },
-      { answerText: 'יש ריבים ', isCorrect: true },
-    ],
-  },
-
-  {
-    questionText: 'בן זוגי לרוב מודה כשהוא טועה',
-    answerOptions: [
-      { answerText: 'מודה', isCorrect: false },
-      { answerText: 'לא מודה', isCorrect: true },
-    ],
-  },
-
-  {
-    questionText: 'בן זוגי מדבר בלי בעיה על דברים שגורמים לו להראות לא טוב',
-    answerOptions: [
-      { answerText: 'אין לו בעיה לדבר', isCorrect: false },
-      { answerText: 'יש לו בעיה לדבר', isCorrect: true },
+      { answerText: 'חושד בי', isCorrect: true },
+      { answerText: 'לא חושד בי', isCorrect: false },
     ],
   },
 ];
-
