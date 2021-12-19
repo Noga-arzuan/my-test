eb86b3f22af3a74d5ef2d3061eaecd11338eefd0
Noga-Arzuan <63960775+Noga-arzuan@users.noreply.github.com>
2021-12-19 12:59:38 +0200

    Update baseQuestions.js

--- src/Pages/Menu/baseQuestions.js
+++ src/Pages/Menu/baseQuestions.js
@@ -1,106 +1,15 @@
 export const questions = [
-  {
-    questionText:
-      'בן הזוג שלי מתרגז אם איני מסכימה איתו     ',
+
+
+ {
+    questionText: 'בן הזוג שלי מקבל אותי כמו שאני  ',
     answerOptions: [
-      { answerText: ' אוי ואבוי לי אם לא נסכים', isCorrect: true },
-      { answerText: ' פתוח לשמוע את דעתי ', isCorrect: false },
+      { answerText: ' מלא ביקורת כלפיי' , isCorrect: false },
+      { answerText: 'מקבל באהבה ', isCorrect: true },
     ],
   },
 
-  {
-    questionText: 'בן זוגי גורם לי אשמה ובושה לעיתים קרובות',
-    answerOptions: [
-      { answerText: 'כל הזמן', isCorrect: true },
-      { answerText: 'ממש לא  ', isCorrect: false },
-    ],
-  },
-
-  {
-    questionText: 'אין לי סיכוי להצליח בלי בן הזוג שלי ',
-    answerOptions: [
-      { answerText: 'מצליחה רק בזכותו' , isCorrect: false },
-      { answerText: 'ההצלחה שלי לא תלויה בו ', isCorrect: true },
-    ],
-  },
-
-  {
-    questionText: 'כשאני מצליחה בן הזוג שלי...',
-    answerOptions: [
-      { answerText: 'מזלזל בי ', isCorrect: true },
-      { answerText: '  מפרגן לי ', isCorrect: false },
-    ],
-  },
-
-  {
-    questionText:
-      'לבן הזוג שלי חשוב לשמוע את דעתי ',
-    answerOptions: [
-      { answerText: ' לא חשוב לו בכלל', isCorrect: true },
-      { answerText: 'חשוב לו מאוד ', isCorrect: false },
-    ],
-  },
-
-  {
-    questionText:
-      'בן הזוג שלי מעודד אותי לעשות דברים שלא קשורים לזוגיות שלנו',
-    answerOptions: [
-      { answerText: ' אוהב שאני רק איתו', isCorrect: true },
-      { answerText: 'מעודד לגמרי', isCorrect: false },
-    ],
-  },
-
-  {
-    questionText:
-      'בן הזוג שלי מתעקש לבוא איתי כשאני נפגשת עם חברות, ידידים או המשפחה הקרובה שלי',
-    answerOptions: [
-      { answerText: 'מתעקש', isCorrect: true },
-      { answerText: 'לא מתעקש', isCorrect: false },
-    ],
-  },
-
- 
-
-  {
-    questionText:
-     'בן הזוג שלי מקנא לי',
-    answerOptions: [
-      { answerText: 'סומך עליי בעיניים עצומות', isCorrect: false },
-      { answerText: 'כל הזמן ועל כל דבר ', isCorrect: true },
-    ],
-  },
-  {
-    questionText:'אני נפגשת עם חברים גם בלי בן הזוג שלי',
-
-    answerOptions: [
-      { answerText: ' תמיד איתו ', isCorrect: true },
-      { answerText: 'איך שבא לי ', isCorrect: false },
-    ],
-  },
   
-   
-  {
-    questionText:
-      'אני מרגישה שאני יכולה להצליח בהרבה  דברים בלי בן הזוג שלי ',
-    answerOptions: [
-      { answerText: 'יכולה להצליח', isCorrect: false },
-      { answerText: 'לא יכולה להצליח', isCorrect: true },
-    ],
-  },
-  {
-    questionText: 'בן זוגי אינו מודה שהוא טועה',
-    answerOptions: [
-      { answerText: ' מודה ומתנצל', isCorrect: false },
-      { answerText: 'אינו מודה לעולם', isCorrect: true },
-    ],
-  },
-  {
-    questionText: 'בן הזוגי שלי מתרגז אם איני מסכימה איתו',
-    answerOptions: [
-      { answerText: ' פתוח לשמוע את דעתי', isCorrect: false },
-      { answerText: 'אוי ואבוי אם לא נסכים', isCorrect: true },
-    ],
-  },
 
   {
     questionText: 'בן הזוג מפרגן לי על ההצלחות שלי',
@@ -114,6 +23,64 @@ export const questions = [
     answerOptions: [
       { answerText: 'חושד בי', isCorrect: true },
       { answerText: 'לא חושד בי', isCorrect: false },
+    ],
+  },
+ {
+    questionText: ' יש לי זמן להתעסק בעוד דברים פרט לזוגיות(משפחה, חברות, חוגים וכו) ',
+    answerOptions: [
+      { answerText: 'משקיעה גם בעצמי', isCorrect: true },
+      { answerText: '  מושקעת בעיקר בזוגיות', isCorrect: false },
+    ],
+  },
+
+  {
+    questionText: 'אני יודעת שלא משנה מה אעשה, בן הזוג שלי יתמוך בי',
+    answerOptions: [
+      { answerText: ' תומך לגמרי', isCorrect: false },
+      { answerText: ' לא תומך בכלל', isCorrect: true },
+    ],
+  },
+
+ 
+
+  {
+    questionText: 'בן הזוג שלי מתקשר ושולח המון הודעות במשך היום',
+    answerOptions: [
+      { answerText: 'מגזים לגמרי', isCorrect: true },
+      { answerText: 'לא במיוחד', isCorrect: false },
+    ],
+  },
+
+  {
+    questionText: '   אני ובן זוגי רבים הרבה ',
+    answerOptions: [
+      { answerText: ' רבים מידי פעם', isCorrect: false },
+      { answerText: ' ריבים על כל דבר ', isCorrect: true },
+    ],
+  },
+
+  {
+    questionText: 'בן זוגי לא מודה שהוא טועה',
+    answerOptions: [
+      { answerText: ' מודה ומתנצל', isCorrect: false },
+      { answerText: 'לא מודה לעולם', isCorrect: true },
+    ],
+  },
+
+ 
+  
+  {
+    questionText: 'כשהיכרנו בן הזוג שלי חיזר אחריי בעדינות',
+    answerOptions: [
+      { answerText: 'בעדינות', isCorrect: false },
+      { answerText: 'באובססיביות', isCorrect: true },
+    ],
+  },
+  {
+    questionText: ' בן זוגי לא מסוגל לקבל ביקורת ',
+    answerOptions: [
+      { answerText: 'דווקא מקשיב ומפנים', isCorrect: false },
+      { answerText: 'לא מסוגל לשמוע', isCorrect: true },
     ],
   },
 ];
