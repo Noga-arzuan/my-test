79d9d20d0f6570cae92bc18101ee2bf3fad05cfd
noga <nogaarzuan@gmail.com>
2022-08-08 22:53:19 +0300

    font

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,23 +1,12 @@
-import React from 'react';
-import { ShareBar } from './ShareBar';
-
-
-
-
-
+import React from "react";
+import { ShareBar } from "./ShareBar";
 
 function Footer() {
-    return (
-        <div classNme="footer-copyright">
-            <ShareBar />
-            <h6>      Copyright © 2021 Wings. All rights reserved </h6>
-
-
-        </div>
-
-
-
-
-    )
+  return (
+    <div classNme="footer-copyright">
+      <ShareBar />
+      <h6 className="footer">Copyright © 2021 Wings. All rights reserved</h6>
+    </div>
+  );
 }
 export default Footer;
