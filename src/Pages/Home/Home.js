4875caf2b5c6f32312d9cd257697ea22f1f7378b
noga <nogaarzuan@gmail.com>
2022-08-15 17:28:28 +0300

    adjust

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,12 +5,12 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div
-      style={{ display: "flex", alignItems: "center", flexDirection: "column" }}
+    // style={{ display: "flex", alignItems: "center", flexDirection: "column" }}
     >
       <div className="share1">
         <h6> הכנו עבורך שאלון קצר שינסה לענות לך על השאלה </h6>
         <h1 className="menue">האם אני נמצאת במערכת יחסים בריאה?</h1>
-        <h1 className="menu">שמחות שבאת</h1>
+        {/* <h1 className="menu">שמחות שבאת</h1> */}
 
         <button className="wingsBtn">
           <Link to="Menu">בואי נתחיל!</Link>
