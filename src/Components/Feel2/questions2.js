5399067cc17f4f2e28c9062dc182ab6e0669e641
noga <nogaarzuan@gmail.com>
2022-09-04 11:45:16 +0300

    div position

--- src/Components/Feel2/questions2.js
+++ src/Components/Feel2/questions2.js
@@ -1,110 +1,105 @@
 export const questions = [
-  {
-    questionText:
-      ' בן הזוג שלי מתקשר אלי ושולח לי הודעות רבות במשך היום',
-    answerOptions: [
-      { answerText: 'מגזים לגמרי', isCorrect: true },
-      { answerText: 'לא במיוחד ', isCorrect: false },
-    ],
-  },
-
-  {
-    questionText:
-      'לבן זוג שלי מפריע שאני משקיעה בעצמי   ',
-
-    answerOptions: [
-      { answerText: 'משתגע מזה', isCorrect: true },
-      { answerText: ' הכי מפרגן ', isCorrect: false },
-    ],
-  },
-
-  {
-    questionText:
-      'שום דבר שאני עושה לא מספיק טוב בשבילו',
-    answerOptions: [
-      { answerText: 'דווקא מעריך', isCorrect: false },
-      { answerText: 'מתלונן כל הזמן ', isCorrect: true },
-    ],
-  },
-
-  {
-    questionText: 'בן הזוג שלי לא מזלזל במה שאני מרגישה',
-    answerOptions: [
-      { answerText: 'אף פעם לא ', isCorrect: false },
-      { answerText: 'כל הזמן ', isCorrect: true },
-    ],
-  },
-
-  {
-    questionText: 'אני מרגישה פחות חופשייה מאז שאנו יחד',
-    answerOptions: [
-      { answerText: ' מרגישה כמו בכלא', isCorrect: true },
-      { answerText: 'חופשיה כמו ציפור ', isCorrect: false },
-    ],
-  },
-
-  {
-    questionText:
-      'בכל הנוגע לקשר, בן זוגי מקבל את כל ההחלטות  ',
-    answerOptions: [
-      { answerText: 'מחליטים ביחד', isCorrect: false },
-      { answerText: 'לא שואל לדעתי ', isCorrect: true },
-    ],
-  },
-
-  {
-    questionText: 'בן הזוג שלי מעליב אותי',
-    answerOptions: [
-      { answerText: 'לעיתים קרובות', isCorrect: true },
-      { answerText: 'אף פעם לא', isCorrect: false },
-    ],
-  },
-
-  {
-    questionText: 'בן הזוג שלי יורד עליי או גורם לי להרגיש קטנה',
-    answerOptions: [
-      { answerText: 'יורד עלי', isCorrect: true },
-      { answerText: 'להיפך,מעצים אותי ', isCorrect: false },
-    ],
-  },
-
-  {
-    questionText:
-      'לעיתים קרובות בן זוגי גורם לי לפקפק בעצמי',
-    answerOptions: [
-      { answerText: 'כל הזמן ', isCorrect: true },
-      { answerText: 'ממש לא ', isCorrect: false },
-    ],
-  },
-  {
-    questionText: 'בן הזוג שלי מזלזל במה שאני מרגישה ',
-    answerOptions: [
-      { answerText: 'ממש לא ', isCorrect: false },
-      { answerText: ' כל הזמן', isCorrect: true },
-    ],
-  },
-  {
-    questionText: 'בן הזוג שלי בודק הרבה פעמים עם מי אני נפגשת או מדברת',
-    answerOptions: [
-      { answerText: 'בודק', isCorrect: true },
-      { answerText: 'לא בודק ', isCorrect: false },
-    ],
-  },
-  {
-    questionText:
-      ' בן זוגי עוקב אחרי ההודעות והמיילים שלי  ',
-    answerOptions: [
-      { answerText: 'יש לי פרטיות מוחלטת ', isCorrect: false },
-      { answerText: 'אין לי טיפת פרטיות ', isCorrect: true },
-    ],
-  },
-  {
-  //   questionText: 'בן הזוג מאוכזב ולא מרוצה ממני לעתים קרובות',
+  // {
+  //   questionText:
+  //     ' בן הזוג שלי מתקשר אלי ושולח לי הודעות רבות במשך היום',
   //   answerOptions: [
-  //     {
-  //       answerText: 'מאוכזב', isCorrect: true
-  //     },
-  //     { answerText: 'לא מאוכזב ', isCorrect: false },
+  //     { answerText: 'מגזים לגמרי', isCorrect: true },
+  //     { answerText: 'לא במיוחד ', isCorrect: false },
   //   ],
+  // },
+
+  {
+    questionText: "לבן זוג שלי מפריע שאני משקיעה בעצמי   ",
+
+    answerOptions: [
+      { answerText: "משתגע מזה", isCorrect: true },
+      { answerText: " הכי מפרגן ", isCorrect: false },
+    ],
+  },
+
+  {
+    questionText: "שום דבר שאני עושה לא מספיק טוב בשבילו",
+    answerOptions: [
+      { answerText: "דווקא מעריך", isCorrect: false },
+      { answerText: "מתלונן כל הזמן ", isCorrect: true },
+    ],
+  },
+
+  {
+    questionText: "בן הזוג שלי לא מזלזל במה שאני מרגישה",
+    answerOptions: [
+      { answerText: "אף פעם לא ", isCorrect: false },
+      { answerText: "כל הזמן ", isCorrect: true },
+    ],
+  },
+
+  {
+    questionText: "אני מרגישה פחות חופשייה מאז שאנו יחד",
+    answerOptions: [
+      { answerText: " מרגישה כמו בכלא", isCorrect: true },
+      { answerText: "חופשיה כמו ציפור ", isCorrect: false },
+    ],
+  },
+
+  {
+    questionText: "בכל הנוגע לקשר, בן זוגי מקבל את כל ההחלטות  ",
+    answerOptions: [
+      { answerText: "מחליטים ביחד", isCorrect: false },
+      { answerText: "לא שואל לדעתי ", isCorrect: true },
+    ],
+  },
+
+  {
+    questionText: "בן הזוג שלי מעליב אותי",
+    answerOptions: [
+      { answerText: "לעיתים קרובות", isCorrect: true },
+      { answerText: "אף פעם לא", isCorrect: false },
+    ],
+  },
+
+  {
+    questionText: "בן הזוג שלי יורד עליי או גורם לי להרגיש קטנה",
+    answerOptions: [
+      { answerText: "יורד עלי", isCorrect: true },
+      { answerText: "להיפך,מעצים אותי ", isCorrect: false },
+    ],
+  },
+
+  {
+    questionText: "לעיתים קרובות בן זוגי גורם לי לפקפק בעצמי",
+    answerOptions: [
+      { answerText: "כל הזמן ", isCorrect: true },
+      { answerText: "ממש לא ", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "בן הזוג שלי מזלזל במה שאני מרגישה ",
+    answerOptions: [
+      { answerText: "ממש לא ", isCorrect: false },
+      { answerText: " כל הזמן", isCorrect: true },
+    ],
+  },
+  {
+    questionText: "בן הזוג שלי בודק הרבה פעמים עם מי אני נפגשת או מדברת",
+    answerOptions: [
+      { answerText: "בודק", isCorrect: true },
+      { answerText: "לא בודק ", isCorrect: false },
+    ],
+  },
+  {
+    questionText: " בן זוגי עוקב אחרי ההודעות והמיילים שלי  ",
+    answerOptions: [
+      { answerText: "יש לי פרטיות מוחלטת ", isCorrect: false },
+      { answerText: "אין לי טיפת פרטיות ", isCorrect: true },
+    ],
+  },
+  {
+    //   questionText: 'בן הזוג מאוכזב ולא מרוצה ממני לעתים קרובות',
+    //   answerOptions: [
+    //     {
+    //       answerText: 'מאוכזב', isCorrect: true
+    //     },
+    //     { answerText: 'לא מאוכזב ', isCorrect: false },
+    //   ],
   },
 ];
